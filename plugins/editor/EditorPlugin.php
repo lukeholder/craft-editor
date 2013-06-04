4297015fc64b63537fd91ddfe9693c4b71f9eff2
Luke Holder <lukemh@gmail.com>
2013-06-04 14:07:20 +0800

    cleanup

--- plugins/editor/EditorPlugin.php
+++ plugins/editor/EditorPlugin.php
@@ -21,50 +21,11 @@ class EditorPlugin extends BasePlugin
 
     public function getDeveloperUrl()
     {
-        return 'http://logicandpixels.com.au';
+        return 'http://patchworks.io';
     }
 
     public function hasCpSection()
     {
         return false;
-    }
-
-    /**
-     * Register control panel routes
-     */
-    // public function hookRegisterCpRoutes()
-    // {
-    //     return array(
-    //         'editor\/ingredients\/new' => 'editor/ingredients/_edit',
-    //         'editor\/ingredients\/(?P<ingredientId>\d+)' => 'editor/ingredients/_edit',
-    //     );
-    // }
-
-    /**
-     * Register twig extension
-     */
-    // public function hookAddTwigExtension()
-    // {
-    //     Craft::import('plugins.editor.twigextensions.EditorTwigExtension');
-
-    //     return new EditorTwigExtension();
-    // }
-
-    /**
-     * Add default ingredients after plugin is installed
-     */
-    public function onAfterInstall()
-    {
-        // $ingredients = array(
-        //     array('name' => 'Gin'),
-        //     array('name' => 'Tonic'),
-        //     array('name' => 'Lime'),
-        //     array('name' => 'Soda'),
-        //     array('name' => 'Vodka'),
-        // );
-
-        // foreach ($ingredients as $ingredient) {
-        //     craft()->db->createCommand()->insert('editor_ingredients', $ingredient);
-        // }
     }
 }
